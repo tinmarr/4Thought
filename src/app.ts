7e285675b78859d60a343c8d4e864927ddd557ac
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-04-23 18:53:53 -0700

    Format

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const app = express();
 const port = 8000;
 
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
 
-app.use('/quill_scripts', express.static(path.join(__dirname, '../node_modules/quill/dist')));
-app.use('/scripts', express.static(path.join(__dirname, '../dist')));
+app.use("/quill_scripts", express.static(path.join(__dirname, "../node_modules/quill/dist")));
+app.use("/scripts", express.static(path.join(__dirname, "../dist")));
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
 });
 
-app.get('/editor', (req, res) => {
-    res.render('editor', { title: 'Editor' });
+app.get("/editor", (req, res) => {
+    res.render("editor", { title: "Editor" });
 });
 
 app.listen(port, () => {

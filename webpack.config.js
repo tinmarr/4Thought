65ae122368ecb0756f83389f1c75b75dc81ae6f7
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-04-23 20:11:40 -0700

    UGH

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
-const path = require('path');
-const mode = 'development'; // development or production
+const path = require("path");
+const mode = "production"; // development or production
 const watch = true; // boolean
-const devtool = 'eval-source-map';
 
 module.exports = [
     {
         mode: mode,
         watch: watch,
         watchOptions: {
-            ignored: ['**/node_modules', '**/**/*.ts'],
+            ignored: ["**/node_modules", "**/**/*.ts"],
         },
-        entry: './dist/quill.js',
+        entry: "./dist/quill.js",
         output: {
-            filename: 'quill.bundle.js',
-            path: path.join(__dirname, './dist'),
+            filename: "quill.bundle.js",
+            path: path.join(__dirname, "./dist"),
+            library: { name: "QuillFile" },
         },
-        devtool: devtool,
     },
 ];

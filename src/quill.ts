0d96ca8100fcdea996440ff3185f80cb4849afd7
Tomer Sedan <tomer.sedan@protonmail.com>
2021-04-25 12:01:18 -0700

    change toolbar structure

--- src/quill.ts
+++ src/quill.ts
@@ -2,28 +2,10 @@ import Quill from "quill";
 const { deltaToMarkdown } = require("quill-delta-to-markdown");
 import { MarkdownToQuill } from "md-to-quill-delta";
 
-let tools: any[][] = [
-    [{ header: [] }, { align: [] }],
-    ["bold", "italic", "underline", "strike", "link"],
-    [{ color: [] }, { background: [] }],
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    ["image"],
-    ['blockquote', 'code-block'],
-];
-
 let quill: Quill = new Quill("#editor", {
-    modules: { toolbar: { container: tools } },
+    modules: { toolbar: "#toolbar" },
     placeholder: "start typing...",
     theme: "snow",
-});
-
-// customize the color tool handler
-quill.getModule("toolbar").addHandler("color", (value: string) => {
-    // if the user clicked the custom-color option, show a prompt window to get the color
-    if (value == "custom-color") {
-        value = prompt("Enter Hex/RGB/RGBA") || "";
-    }
-    quill.format("color", value);
 });
 
 function toMarkdown(): void {

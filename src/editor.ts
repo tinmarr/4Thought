6a748290181ba50297e6dbb1f22dbe608cf63222
Tomer Sedan <tomer.sedan@protonmail.com>
2021-04-28 15:02:40 -0700

    defer editor script

--- src/editor.ts
+++ src/editor.ts
@@ -15,24 +15,22 @@ const data = JSON.parse(document.currentScript?.getAttribute("doc-data")!);
 
 quill.setContents(data.delta);
 
-window.onload = () => {
-    if (data.name != null && data.name != "untitled note") (document.getElementById("notename") as HTMLInputElement).value = data.name;
+if (data.name != null && data.name != "untitled note") (document.getElementById("notename") as HTMLInputElement).value = data.name;
 
-    const syncBtn = document.getElementById("sync-btn")!;
-    syncBtn.onclick = function () {
-        if (!syncBtn.classList.contains("rotating")) {
-            syncBtn.classList.add("rotating");
-            setTimeout(() => {
-                syncBtn.classList.remove("rotating");
-            }, 1000);
-        }
+const syncBtn = document.getElementById("sync-btn")!;
+syncBtn.onclick = function () {
+    if (!syncBtn.classList.contains("rotating")) {
+        syncBtn.classList.add("rotating");
+        setTimeout(() => {
+            syncBtn.classList.remove("rotating");
+        }, 1000);
+    }
 
-        const noteName = (<HTMLInputElement>document.getElementById("notename")).value || "untitled note";
-        let data = { id: identifier, name: noteName, delta: quill.getContents() };
-        send("/save", data, (res: any) => {
-            console.log(res);
-        });
-    };
+    const noteName = (<HTMLInputElement>document.getElementById("notename")).value || "untitled note";
+    let data = { id: identifier, name: noteName, delta: quill.getContents() };
+    send("/save", data, (res: any) => {
+        console.log(res);
+    });
 };
 
 declare global {

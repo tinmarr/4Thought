1cc161f2de5bba8e1ea40a33f3694e30494ff094
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-05-14 17:58:22 -0700

    Nice Lookin

--- src/widgets.ts
+++ src/widgets.ts
@@ -1,6 +1,7 @@
 class Widget {
     private element: HTMLDivElement;
     private coords: { top: string; left: string } = { top: "0px", left: "0px" };
+    private dims: { height: number; width: number } = { height: 0, width: 0 };
     private poppedOut: boolean;
     private parent: HTMLElement;
 
@@ -22,18 +23,25 @@ class Widget {
         contentDiv.innerHTML = content;
         contentDiv.classList.add("widgetContent");
 
-        let button: HTMLButtonElement = document.createElement("button");
-        button.innerHTML = "POP";
+        let button: HTMLAnchorElement = document.createElement("a");
+        button.classList.add("float-end");
+        button.innerHTML = "<i class='btn fal fa-inbox-out fa-sm p-0 m-0'></i>";
+
         button.onmousedown = () => {
-            console.log("click!");
-            this.generalPop();
+            button.innerHTML = this.generalPop();
         };
 
+        this.element.appendChild(button);
         this.element.appendChild(headerDiv);
         this.element.appendChild(contentDiv);
-        this.element.appendChild(button);
 
         this.parent.appendChild(this.element);
+
+        this.dims.height = this.element.offsetHeight;
+        this.dims.width = this.element.offsetWidth;
+
+        this.coords.top = `${this.parent.offsetTop + this.element.offsetTop}px`;
+        this.coords.left = `${this.parent.offsetLeft + this.element.offsetLeft}px`;
     }
 
     popOut() {
@@ -43,15 +51,18 @@ class Widget {
         this.parent.removeChild(this.element);
         document.body.appendChild(this.element);
         window.dragElement(this.element);
+        this.element.style.width = this.dims.width + "px";
+        this.element.style.height = this.dims.height + "px";
         this.poppedOut = true;
     }
 
-    generalPop() {
+    generalPop(): string {
         if (this.poppedOut) {
             this.popIn();
-            return;
+            return "<i class='btn fal fa-inbox-out fa-sm p-0 m-0'></i>";
         }
         this.popOut();
+        return "<i class='btn fal fa-inbox-in fa-sm p-0 m-0'></i>";
     }
 
     popIn() {

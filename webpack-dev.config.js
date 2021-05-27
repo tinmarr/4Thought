2967ad1b04efeb54b15ad5ba9c7d96f7ff8a33f8
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-05-26 18:57:02 -0700

    Split Config Files

---
+++ webpack-dev.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = [
+    {
+        mode: "development",
+        watch: true,
+        entry: {
+            editor: "./dist/src/editor.js",
+            dragManager: "./dist/src/dragManager.js",
+        },
+        output: {
+            filename: "[name].bundle.js",
+            path: path.join(__dirname, "./dist/src"),
+            library: ["window", "[name]"],
+            libraryTarget: "var",
+        },
+        // devtool: "source-map"
+    },
+];

6d60a21fa65de7b16cbe75600b1b2fd4c4dc6912
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-05-26 18:51:58 -0700

    Refractor Editor.ts

--- src/editor.ts
+++ src/editor.ts
@@ -4,10 +4,12 @@ import BlotFormatter, { ResizeAction, DeleteAction, ImageSpec } from "quill-blot
 import ImageCompress from "quill-image-compress";
 import MagicUrl from "quill-magic-url";
 
+/*** Quill Imports ***/
 Quill.register("modules/blotFormatter", BlotFormatter);
 Quill.register("modules/imageCompress", ImageCompress);
 Quill.register("modules/magicUrl", MagicUrl);
 
+// Image handler
 class CustomImageSpec extends ImageSpec {
     getActions() {
         return [ResizeAction, DeleteAction];
@@ -33,11 +35,43 @@ export const quill: Quill = new Quill("#editor", {
     placeholder: "start typing...",
     theme: "snow",
 });
-new QuillMarkdown(quill, {});
+new QuillMarkdown(quill, {}); // Live Markdown Support
 
+// Function Input Support for Quill
+declare function mathquill4quill(): any;
+let enableMathQuillFormulaAuthoring = mathquill4quill();
+enableMathQuillFormulaAuthoring(quill, {
+    operators: [
+        ["\\frac{x}{y}", "\\frac"],
+        ["\\sqrt[n]{x}", "\\nthroot"],
+        ["\\int_{a}^{b}", "\\int"],
+        ["\\sum^{a}_{b}", "\\sum"],
+        ["\\infty", "\\infty"],
+    ],
+});
+
+// Get saved data
+const identifier: string = document.currentScript?.getAttribute("note-id")!;
+const data = JSON.parse(document.currentScript?.getAttribute("doc-data")!);
+
+quill.setContents(data.delta);
+
+// Initial Processing of Saved Data
+if (data.name != null && data.name != "untitled note") (document.getElementById("notename") as HTMLInputElement).value = data.name;
+
+if (data.widgets != undefined) for (let widget of data.widgets) Widget.generate(widget);
+
+if (!("txtshortcuts" in data)) {
+    data["txtshortcuts"] = new SmallD();
+}
+export let textshortcuts: SmallD = data["txtshortcuts"];
+
+/*** Save Handler ***/
 let lastSavedOn: Date = new Date();
 
 document.addEventListener("keydown", handleKeyPress);
+window.onbeforeunload = save;
+
 function handleKeyPress(e) {
     if ((e.ctrlKey || e.metaKey) && e.code == "KeyS") {
         e.preventDefault();
@@ -45,62 +79,62 @@ function handleKeyPress(e) {
     }
 }
 
-export enum Format {
-    list,
-    raw,
-    stringWithNoN,
-}
-export function getText(format: Format): string[] | string {
-    let thing: HTMLDivElement = <HTMLDivElement>document.getElementsByClassName("ql-editor")[0];
-    let text: string = thing.innerText;
-    // let text: string = quill.getContents().ops[0].insert;
-    // quill.setText
-    if (format === Format.list) {
-        return text.split("\n");
-    }
-    if (format === Format.stringWithNoN) {
-        return text.replace(/(\r\n|\n|\r)/gm, " ");
-    }
-    return text;
+function updateSaveTime(): void {
+    if (lastSavedOn.toDateString() != new Date().toDateString())
+        document.getElementById("save-time")!.innerHTML = "last saved on " + lastSavedOn.toDateString();
+    else document.getElementById("save-time")!.innerHTML = "last saved on " + lastSavedOn.toTimeString();
 }
 
+function nameNote(): void {
+    if ((<HTMLInputElement>document.getElementById("notename")).value == "") {
+        if ((<HTMLElement>document.getElementsByClassName("ql-editor")[0]).innerText != "\n") {
+            let list: string[] = (<HTMLElement>document.getElementsByClassName("ql-editor")[0]).innerText.split(" ");
+            noteName = list.length > 5 ? list.splice(0, 5).join(" ") : list.join(" ");
+        } else {
+            noteName = "Untitled Note";
+        }
+    } else {
+        noteName = (<HTMLInputElement>document.getElementById("notename")).value;
+    }
+}
 
-// export function dothething(){
-//     let editor: HTMLDivElement = <HTMLDivElement>document.getElementsByClassName("ql-editor")[0];
-//     function compareOrRecall(children: HTMLCollection){
-//         for (let i = 0; i < children.length; i++){
-//             let child = <HTMLElement>children[i];
-            
-//             if(!child.innerText){
-//                 console.log(child);
-//                 continue;
-//             }
-//             let words: string[] = child.innerText.split(" ");
-//             for (let i = 0; i < words.length; i++) {
-//                 let element = words[i];
-//                 if (textshortcuts.match(element) != null) {
-//                     console.log(element, textshortcuts.match(element)![0]);
-//                     words[i] = textshortcuts.match(element)![0];
-//                 }
-//             }
-//             let line = words.join(" ");
-//             child.innerText = line;
-            
-//             if (child.children){
-//                 return compareOrRecall(child.children);
-//             }
-             
-//         }
-//     }
-//     let children = editor.children;
-//     compareOrRecall(children);
-// }
-export function dothething(){
+function save(): void {
+    // Update before save
+    lastSavedOn = new Date();
+    updateSaveTime();
+
+    nameNote();
+
+    // Start the icon animation
+    if (!syncBtn.children[0].classList.contains("rotating")) {
+        syncBtn.children[0].classList.add("rotating");
+        setTimeout(() => {
+            syncBtn.children[0].classList.remove("rotating");
+        }, 1000);
+    }
+
+    // Generate List of Widgets
+    let widgetList: object[] = Widget.widgets.map((widg: Widget) => {
+        return widg.toObj();
+    });
+
+    // Generate Object and Send to Backend
+    let data = {
+        id: identifier,
+        name: noteName,
+        delta: quill.getContents(),
+        widgets: widgetList,
+        txtshortcuts: textshortcuts,
+    };
+    sendNoCB("/save", data);
+}
+
+export function dothething() {
     let editorcontents = quill.getContents();
-    for(let i = 0; i < editorcontents.ops.length; i++) {
+    for (let i = 0; i < editorcontents.ops.length; i++) {
         let element = editorcontents.ops[i];
         // console.log(i, " ", typeof element.insert == typeof " ", " ", element.insert);
-        if(element.insert && typeof element.insert == typeof " ") {
+        if (element.insert && typeof element.insert == typeof " ") {
             let words: string[] = (element.insert as string).split(" ");
             for (let i = 0; i < words.length; i++) {
                 let wordsmightcontainbackslash = words[i].split("\n");
@@ -117,50 +151,12 @@ export function dothething(){
             element.insert = line;
             // console.log(line, " ", element.insert);
         }
-        
     }
-    quill.setContents(editorcontents);    
+    quill.setContents(editorcontents);
 }
 
-// recursively call the children of the editor until  all we have is the text, then each individual text check on the dictionary
-export function insertText(format: Format, newText: any) {
-    let thing: HTMLDivElement = <HTMLDivElement>document.getElementsByClassName("ql-editor")[0];
-    let text: string = thing.innerText;
-    if (format === Format.list) {
-        thing.innerText = newText.join("\n");
-        return;
-    }
-    if (format === Format.stringWithNoN) {
-        return "cannot reinsert this";
-    }
-    thing.innerText = text;
-}
-declare function mathquill4quill(): any;
-let enableMathQuillFormulaAuthoring = mathquill4quill();
-enableMathQuillFormulaAuthoring(quill, {
-    operators: [
-        ["\\frac{x}{y}", "\\frac"],
-        ["\\sqrt[n]{x}", "\\nthroot"],
-        ["\\int_{a}^{b}", "\\int"],
-        ["\\sum^{a}_{b}", "\\sum"],
-        ["\\infty", "\\infty"],
-    ],
-});
-
-const identifier: string = document.currentScript?.getAttribute("note-id")!;
-const data = JSON.parse(document.currentScript?.getAttribute("doc-data")!);
-
-quill.setContents(data.delta);
-
-if (data.name != null && data.name != "untitled note") (document.getElementById("notename") as HTMLInputElement).value = data.name;
-
-if (data.widgets != undefined) for (let widget of data.widgets) Widget.generate(widget);
-
-if (!("txtshortcuts" in data)) {
-    data["txtshortcuts"] = new SmallD();
-}
-export let textshortcuts: SmallD = data["txtshortcuts"];
-
+/*** Button Handlers ***/
+// Texting Shortcuts Button
 const dictionaryBtn = document.getElementById("add-texting-shortcuts")!;
 let content = document.getElementById("text-shortcuts-popover")?.innerHTML;
 let options = {
@@ -175,8 +171,6 @@ let ppover = new window.bootstrap.Popover(dictionaryBtn, options);
 dictionaryBtn.onclick = () => {
     ppover.toggle();
 };
-let textingToggleState: boolean = false;
-
 dictionaryBtn.addEventListener("shown.bs.popover", (e) => {
     const textingsubmitBtn = <HTMLAnchorElement>document.querySelector("div.popover-body > #SubmitNewDefinition")!;
     //console.log(textingsubmitBtn);
@@ -189,48 +183,20 @@ dictionaryBtn.addEventListener("shown.bs.popover", (e) => {
         textshortcuts.addPair(stuffin, stuffout);
         //console.log(textshortcuts);
     };
-    const textingToggle = <HTMLAnchorElement>document.querySelector("div.popover-body > #txtModeToggle")!;
-    textingToggle.onclick = function () {
-        console.log("still alive.");
-        textingToggleState = !textingToggleState;
-        if (textingToggleState)
-        dothething();
-        //console.log(textingToggle);
-        //console.log(`toggled to ${textingToggleState.valueOf()}`);
-    };
 });
 
-
-
-// function scanLine(line: string) {
-//     let words: string[] = line.split(" ");
-//     for (let i = 0; i < words.length; i++) {
-//         let element = words[i];
-//         if (textshortcuts.match(element) !== null) {
-//             console.log(element, textshortcuts.match(element)[0]);
-//             words[i] = textshortcuts.match(element)[0];
-//         }
-//     }
-//     line = words.join(" ");
-//     console.log(line);
-//     return line;
-// }
-// document.addEventListener("keyup", function (event) {
-//     let eventsForCheck: string[] = ["Enter", "Space", "Period", "Slash"];
-//     eventsForCheck.forEach((eventType) => {
-//         if (event.code == eventType) {
-//             console.log("chekcing...");
-//             let text: string[] = getText(Format.list);
-//             let last = text[text.length - 1];
-//             // let secondlast = text.pop(); // this may cause error
-//             text[text.length - 1] = scanLine(last);
-//             insertText(Format.list, text);
-//         }
-//     });
-// });
+// Toggle whether or not you want the shortcuts
+const textingToggle = document.getElementById("txtModeToggle")!;
+let textingToggleState: boolean = false;
+textingToggle.onchange = function () {
+    const inpt = document.getElementsByName("input-texting-toggle")[0]! as HTMLInputElement;
+    textingToggleState = inpt.checked;
+    dothething();
+};
 
 let noteName: string = (<HTMLInputElement>document.getElementById("notename")).value;
 
+// Info tooltip
 const infoBtn = document.getElementById("info-btn")!,
     infoTTip = document.getElementById("infottip")!;
 let infoToggle = true;
@@ -242,9 +208,11 @@ infoBtn.onclick = function () {
     infoToggle = !infoToggle;
 };
 
+// Save the document
 const syncBtn = document.getElementById("sync-btn")!;
 syncBtn.onclick = save;
 
+// Close the document
 const closeBtn: HTMLLinkElement = <HTMLLinkElement>document.getElementById("close")!;
 closeBtn.onclick = (e) => {
     e.preventDefault();
@@ -252,52 +220,7 @@ closeBtn.onclick = (e) => {
     window.location.href = closeBtn.href;
 };
 
-function updateSaveTime(): void {
-    if (lastSavedOn.toDateString() != new Date().toDateString())
-        document.getElementById("save-time")!.innerHTML = "last saved on " + lastSavedOn.toDateString();
-    else document.getElementById("save-time")!.innerHTML = "last saved on " + lastSavedOn.toTimeString();
-}
-
-function save(): void {
-    lastSavedOn = new Date();
-    updateSaveTime();
-
-    nameNote();
-
-    if (!syncBtn.children[0].classList.contains("rotating")) {
-        syncBtn.children[0].classList.add("rotating");
-        setTimeout(() => {
-            syncBtn.children[0].classList.remove("rotating");
-        }, 1000);
-    }
-
-    let widgetList: object[] = Widget.widgets.map((widg: Widget) => {
-        return widg.toObj();
-    });
-    console.log(textshortcuts);
-    let data = {
-        id: identifier,
-        name: noteName,
-        delta: quill.getContents(),
-        widgets: widgetList,
-        txtshortcuts: textshortcuts,
-    };
-    sendNoCB("/save", data);
-}
-
-function nameNote(): void {
-    if ((<HTMLInputElement>document.getElementById("notename")).value == "") {
-        if ((<HTMLElement>document.getElementsByClassName("ql-editor")[0]).innerText != "\n") {
-            let list: string[] = (<HTMLElement>document.getElementsByClassName("ql-editor")[0]).innerText.split(" ");
-            noteName = list.length > 5 ? list.splice(0, 5).join(" ") : list.join(" ");
-        } else {
-            noteName = "Untitled Note";
-        }
-    } else {
-        noteName = (<HTMLInputElement>document.getElementById("notename")).value;
-    }
-}
-
+// Download the document
 const downloadButton = document.getElementById("downloadPDF")!;
 downloadButton.onclick = () => {
     nameNote();
@@ -309,6 +232,7 @@ downloadButton.onclick = () => {
         .save(noteName + ".pdf");
 };
 
+// Text to Speech
 const ttsButton = document.getElementById("speakSelection")!;
 ttsButton.onclick = () => {
     if (quill.getSelection() != null && !window.speechSynthesis.speaking) {
@@ -321,53 +245,37 @@ ttsButton.onclick = () => {
     }
 };
 
-const wikiLookup = document.getElementById("searchWiki")!;
-wikiLookup.onclick = () => {
+// Wikipedia Search
+document.getElementById("searchWiki")!.onclick = () => {
     if (quill.getSelection() != null) {
         searchWikipedia(quill.getText(quill.getSelection()?.index, quill.getSelection()?.length));
     }
 };
 
-const dicLookup = document.getElementById("searchDic")!;
-dicLookup.onclick = () => {
+// Dictionary Search
+document.getElementById("searchDic")!.onclick = () => {
     if (quill.getSelection() != null) {
         searchDictionary(quill.getText(quill.getSelection()?.index, quill.getSelection()?.length), "en_US");
     }
 };
 
-const newComment = document.getElementById("newComment")!;
-newComment.onclick = () => {
+// Add Comment Widget
+document.getElementById("newComment")!.onclick = () => {
     new CommentWidget();
 };
 
-const newYoutube = document.getElementById("newYoutube")!;
-newYoutube.onclick = () => {
+// Add Youtube Widget
+document.getElementById("newYoutube")!.onclick = () => {
     new YoutubeWidget();
 };
 
-const newRecording = document.getElementById("newRecording")!;
-newRecording.onclick = () => {
+// Add Recording Widget
+document.getElementById("newRecording")!.onclick = () => {
     new RecordWidget();
 };
 
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]#add-texting-shortcuts'));
-let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    let content = document.getElementById("text-shortcuts-popover")?.innerHTML;
-    let options = {
-        container: "body",
-        sanitize: false,
-        html: true,
-        placement: "bottom",
-        content: content,
-        trigger: "click",
-    };
-    return new window.bootstrap.Popover(popoverTriggerEl, options);
-});
-
-window.onbeforeunload = (e: BeforeUnloadEvent) => {
-    save();
-};
-
+/*** API Calls ***/
+// Wikipedia Search Manager
 function searchWikipedia(keyWord: string) {
     let url = "https://en.wikipedia.org/w/api.php";
     let params = {
@@ -395,6 +303,7 @@ function searchWikipedia(keyWord: string) {
         });
 }
 
+// Dictionary Search Manager
 function searchDictionary(word: string, language: "en_US" | "fr" | "es") {
     let url = "https://api.dictionaryapi.dev/api/v2/entries";
 
@@ -414,6 +323,7 @@ function searchDictionary(word: string, language: "en_US" | "fr" | "es") {
         });
 }
 
+// Nicer Alert
 function alert(text: string) {
     let element = document.createElement("div");
     element.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show", "m-0");
@@ -422,6 +332,7 @@ function alert(text: string) {
 
     document.body.insertBefore(element, document.body.firstChild);
 }
+window.alert = alert;
 
 declare global {
     interface Window {
